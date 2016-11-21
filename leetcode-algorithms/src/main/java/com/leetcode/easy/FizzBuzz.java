36ebc3f6fd9003aa72ce0613a97dab8a917972db
qiaodaimadelaowang <tinyhui.wang@gmail.com>
2016-11-21 11:37:57 +0800

    LeetCode:Island Perimeter(not completed).

--- leetcode-algorithms/src/main/java/com/leetcode/easy/FizzBuzz.java
+++ leetcode-algorithms/src/main/java/com/leetcode/easy/FizzBuzz.java
@@ -41,7 +41,7 @@ import java.util.List;
 
 public class FizzBuzz {
 
-    public List<String> fizzBuzz(int n) {
+    public List<String> solution(int n) {
         List<String> msg = new ArrayList<String>(n);
         for (int i = 1; i <= n; i++) {
             if (i % 3 != 0 && i % 5 != 0) {
@@ -59,7 +59,7 @@ public class FizzBuzz {
 
     public static void main(String[] args) {
         FizzBuzz fb = new FizzBuzz();
-        System.out.println(fb.fizzBuzz(15));
+        System.out.println(fb.solution(15));
 //        System.out.println(fb.fizzBuzz(Integer.valueOf(args[0])));
     }
 
---
+++ leetcode-algorithms/src/main/java/com/leetcode/easy/IslandPerimeter.java
@@ -0,0 +1,43 @@
+package com.leetcode.easy;
+
+/**
+ * Created with IntelliJ IDEA.
+ * <p>
+ * Description: https://leetcode.com/problems/island-perimeter/
+ * <p>
+ * You are given a map in form of a two-dimensional integer grid where 1 represents land and 0 represents water.
+ * <p>
+ * Grid cells are connected horizontally/vertically (not diagonally).
+ * <p>
+ * The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
+ * <p>
+ * The island doesn't have "lakes" (water inside that isn't connected to the water around the island).
+ * <p>
+ * One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100.
+ * <p>
+ * Determine the perimeter of the island.
+ * <p>
+ * Example:
+ * <p>
+ * [[0,1,0,0],
+ * [1,1,1,0],
+ * [0,1,0,0],
+ * [1,1,0,0]]
+ * <p>
+ * Answer: 16
+ * <p>
+ * Explanation: The perimeter is the 16 yellow stripes in the image below:
+ * <p>
+ * https://github.com/qiaodaimadelaowang/screenshots/blob/master/island.png?raw=true
+ * <p>
+ * Author: wanghui<tinyhui.wang@gmail.com>
+ * Created on: 2016-11-21 11:23
+ */
+
+public class IslandPerimeter {
+
+    public int solution(int[][] grid) {
+
+        return 0;
+    }
+}

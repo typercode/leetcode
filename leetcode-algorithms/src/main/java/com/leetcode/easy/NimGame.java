b5a29dc6956272ab2fff02493f5dcabf6aa6c29a
qiaodaimadelaowang <tinyhui.wang@gmail.com>
2016-11-22 22:25:33 +0800

    https://leetcode.com/problems/nim-game/

---
+++ leetcode-algorithms/src/main/java/com/leetcode/easy/NimGame.java
@@ -0,0 +1,35 @@
+package com.leetcode.easy;
+
+/**
+ * Created with IntelliJ IDEA.
+ * <p>
+ * Description: Nim Game
+ * <p>
+ * You are playing the following Nim Game with your friend: There is a heap of stones on the table, each time one of you take turns to remove 1 to 3 stones.
+ * <p>
+ * The one who removes the last stone will be the winner.
+ * <p>
+ * You will take the first turn to remove the stones.
+ * <p>
+ * Both of you are very clever and have optimal strategies for the game.
+ * <p>
+ * Write a function to determine whether you can win the game given the number of stones in the heap.
+ * <p>
+ * For example, if there are 4 stones in the heap, then you will never win the game: no matter 1, 2, or 3 stones you remove, the last stone will always be removed by your friend.
+ * <p>
+ * Author: wanghui<tinyhui.wang@gmail.com>
+ * Created on: 2016-11-21 18:21
+ */
+
+public class NimGame {
+
+    public boolean solution(int n) {
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        NimGame ng = new NimGame();
+        ng.solution(2);
+    }
+}

cc0283c952b15b4bd774812df7fcac61a0a56619
qiaodaimadelaowang <tinyhui.wang@gmail.com>
2016-11-24 16:10:55 +0800

    https://leetcode.com/problems/two-sum/

--- leetcode-algorithms/src/main/java/com/leetcode/easy/TwoSum.java
+++ leetcode-algorithms/src/main/java/com/leetcode/easy/TwoSum.java
@@ -27,6 +27,31 @@ package com.leetcode.easy;
 
 public class TwoSum {
 
+    /**
+     * leetcode solution #1(Brute Force)
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    /**
+     * my solution
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
     public int[] solution(int[] nums, int target) {
         int[] indices = new int[2];
         for (int i = 0; i < nums.length; i++) {

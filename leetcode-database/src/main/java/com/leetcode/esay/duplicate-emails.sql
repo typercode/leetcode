5ad6796421768267d7aa2d5d061cedf19548d7e1
qiaodaimadelaowang <tinyhui.wang@gmail.com>
2016-11-29 11:23:26 +0800

    https://leetcode.com/problems/duplicate-emails/

---
+++ leetcode-database/src/main/java/com/leetcode/esay/duplicate-emails.sql
@@ -0,0 +1,27 @@
+-- Write a SQL query to find all duplicate emails in a table named Person.
+--
+-- +----+---------+
+-- | Id | Email   |
+-- +----+---------+
+-- | 1  | a@b.com |
+-- | 2  | c@d.com |
+-- | 3  | a@b.com |
+-- +----+---------+
+-- For example, your query should return the following for the above table:
+--
+-- +---------+
+-- | Email   |
+-- +---------+
+-- | a@b.com |
+-- +---------+
+-- Note: All emails are in lowercase.
+
+
+-- # Write your MySQL query statement below
+SELECT DISTINCT
+    p1.Email
+FROM
+    Person p1,
+    Person p2
+WHERE
+    p1.Email = p2.Email AND p1.id != p2.id;

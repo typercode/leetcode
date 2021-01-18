75f7a53bdfee9f8f0ce5a4858c9abeddf0ae7800
tinyhui.wang <tinyhui.wang@gmail.com>
2021-01-18 16:48:18 +0800

    addTwoNumbers

---
+++ leetcode-algorithms/src/main/java/com/leetcode/easy/AddTwoNumbers.java
@@ -0,0 +1,26 @@
+package com.leetcode.easy;
+
+public class AddTwoNumbers {
+
+  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+    return null;
+  }
+
+  /** Definition for singly-linked list. */
+  public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+      this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+      this.val = val;
+      this.next = next;
+    }
+  }
+}
